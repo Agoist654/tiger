d98195795c8e51e396f80cf17215bf3cbcacf739
David Xu <david.xu@epita.fr>
2023-04-08 21:10:04 +0200

    scope end et scope begin

--- src/bind/binder.hxx
+++ src/bind/binder.hxx
@@ -88,10 +88,6 @@ namespace bind
           // FIXME: Some code was deleted here.
           check_main(e);
           funscope_.put(e.name_get(), &e);
-
-          e.formals_get().accept(*this);
-          if(e.result_get())
-            e.result_get()->accept(*this);
       }
 
   // Compute the bindings of this function's body.
