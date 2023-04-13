2bf3a47b93f883b646b0695d5ae339b033851176
souleymane.meite <souleymane.meite@epita.fr>
2023-04-13 17:19:14 +0200

    lib/misc/singleton.hh

--- lib/misc/singleton.hh
+++ lib/misc/singleton.hh
@@ -5,34 +5,25 @@
 
 #pragma once
 
-#include <string>
-
 namespace misc
 {
-	template <typename T> class Singleton
-	{
-		// FIXME: Some code was deleted here.
-		public:
-			Singleton(const Singleton&) = delete;
-			Singleton(Singleton&&) = delete;
-			Singleton& operator=(const Singleton&) = delete;
-			Singleton& operator=(Singleton&&) = delete;
-			static const Singleton& instance()
-			{
-				static Singleton instance;
-				return instance;
-			}
-		private:
-			Singleton() = default;
-	};
-	template <>
-		Singleton<String>& Singleton<String>::instance()
-		{
-			static const Singleton<String> instance(std::string("string"));
-			return instance;
-		}
+    template <typename T>
+    class Singleton
+    {
+        // FIXME: Some code was deleted here.
+    public:
+        Singleton(const Singleton&) = delete;
+        Singleton(Singleton&&) = delete;
+        Singleton& operator=(const Singleton&) = delete;
+        Singleton& operator=(Singleton&&) = delete;
+        static const Singleton& instance()
+        {
+            static T instance;
+            return instance;
+        }
 
-
-
+    private:
+        Singleton() = default;
+    };
 
 } // namespace misc
